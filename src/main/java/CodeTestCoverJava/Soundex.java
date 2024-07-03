ddaa8523e70f60f706f73a92bbf79b523f187964
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-03 11:40:41 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -2,32 +2,30 @@ package CodeTestCoverJava;
 
 public class Soundex {
 
-    public static String generateSoundex(String name) {
+   public static String generateSoundex(String name) {
         if (name == null || name.isEmpty()) {
             return "";
         }
-
+        name = name.toUpperCase();
         StringBuilder soundex = new StringBuilder();
-        soundex.append(Character.toUpperCase(name.charAt(0)));
-        char prevCode = getSoundexCode(name.charAt(0));
+        soundex.append(name.charAt(0));
+
 
         for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
             char code = getSoundexCode(name.charAt(i));
+            char prevCode = getSoundexCode(name.charAt(i - 1));
             if (code != '0' && code != prevCode) {
                 soundex.append(code);
-                prevCode = code;
             }
         }
 
-        while (soundex.length() < 4) {
+        if (soundex.length() < 4) {
             soundex.append('0');
         }
-
         return soundex.toString();
     }
 
     private static char getSoundexCode(char c) {
-        c = Character.toUpperCase(c);
         switch (c) {
             case 'B': case 'F': case 'P': case 'V':
                 return '1';
