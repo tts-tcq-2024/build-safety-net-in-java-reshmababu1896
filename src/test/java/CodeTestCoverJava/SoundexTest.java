63bd9180fc3c37a50c629cb175c4e217499564b9
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-05 13:57:27 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -41,9 +41,10 @@ public class SoundexTest {
         StringBuilder soundex=new StringBuilder();
         String name="AEIOU";
         soundexMock.generateSoundex(name);
-       // Mockito.verify(soundexMock, Mockito.times(1)).checkLength(soundex,name);
+       // Mockito.verify(soundexMock, Mockito.times(1)).checkLength(soundex, name);
         // verify(soundexMock. Mockito.times(1)).checkLength(soundex,name);
-        Mockito.verify(soundexMock, Mockito.times(1)).checkLength(Mockito.any(StringBuilder.class), Mockito.eq(name));
+        verify(soundexMock, times(1)).checkLength(soundex,name);
+        // Mockito.verify(soundexMock, Mockito.times(1)).checkLength(Mockito.any(StringBuilder.class), Mockito.eq(name));
     }
     
     
